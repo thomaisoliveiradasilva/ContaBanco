aeb126cb80208652845fa4beac660e4943b2705c
thomais oliveira da silva <139508466+thomaisoliveiradasilva@users.noreply.github.com>
2024-06-11 19:51:31 -0300

    Meu desafio esta concluido java aqui vou eu.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,6 +7,7 @@ public class ContaTerminal {
 
         // solicitanod o número da conta
         System.out.println("Digite o número da sua conta!");
+        int numero =scanner.nextInt();
 
         // Limpar o buffer do scanner
         scanner.nextLine();
@@ -18,6 +19,18 @@ public class ContaTerminal {
         // solicitando o nome do cliente
         System.out.println("Digite seu nome!");
         String nome = scanner.nextLine();
+
+        // solicitando o saldo da conta
+        System.out.println("por favor, Digite o quanto você que depositar!");
+        double saldo = scanner.nextDouble();
+
+        // Fechando o scanner
+        scanner.close();
+
+        //Exibindo a mensagem Final
+        System.out.println("Olá senhor " + nome + " Obrigado por criar uma conta em nossso banco, sua agência é " + agencia + " sua conta é " + numero + " e seu saldo é " + saldo + " seu primeiro sague já está disponivel!");
+        
+
 
     }
 }
